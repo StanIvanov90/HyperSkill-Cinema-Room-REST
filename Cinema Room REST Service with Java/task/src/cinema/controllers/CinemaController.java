a4c6ebc7be4f30b27b5f0443fdd47e844bc4eaf5
Stanislav Ivanov <stanislavivanov@Stanislavs-Mac-Studio.local>
2026-04-19 18:58:17 +0300

    feat: add endpoint for booking seats

--- Cinema Room REST Service with Java/task/src/cinema/controllers/CinemaController.java
+++ Cinema Room REST Service with Java/task/src/cinema/controllers/CinemaController.java
@@ -1,9 +1,16 @@
 package cinema.controllers;
 
+import cinema.exceptions.DuplicateEntityException;
+import cinema.exceptions.EntityOutOfBoundsException;
+import cinema.models.Seat;
 import cinema.services.CinemaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class CinemaController {
@@ -16,8 +23,17 @@ public class CinemaController {
 
     @GetMapping("/seats")
     public ResponseEntity<?> getCinemaRoom() {
-            return ResponseEntity.ok(cinemaService.getCinemaRoom());
+        return ResponseEntity.ok(cinemaService.getCinemaRoom());
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<?> bookSeat(@RequestBody Seat seat) {
+        try {
+            return ResponseEntity.ok(cinemaService.bookSeat(seat));
+        } catch (EntityOutOfBoundsException | DuplicateEntityException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
         }
     }
+}
 
 
